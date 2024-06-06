85139bd74f6f053598bc17b6cd35019dd73f398d
P4ver <s.pales20@gmail.com>
2024-06-06 16:00:17 -0700

    ts

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -2,6 +2,7 @@
 '''A module for working with lockboxes.
 '''
 
+
 def canUnlockAll(boxes):
     '''Checks if all the boxes in a list of boxes containing the keys
     (indices) to other boxes can be unlocked given that the first
@@ -10,7 +11,7 @@ def canUnlockAll(boxes):
     total_boxes = len(boxes)
     unlocked_boxes = set([0])
     keys_to_use = set(boxes[0]).difference(set([0]))
-    
+
     while len(keys_to_use) > 0:
         current_key = keys_to_use.pop()
         if not current_key or current_key >= total_boxes or current_key < 0:
@@ -18,6 +19,5 @@ def canUnlockAll(boxes):
         if current_key not in unlocked_boxes:
             keys_to_use = keys_to_use.union(boxes[current_key])
             unlocked_boxes.add(current_key)
-    
-    return total_boxes == len(unlocked_boxes)
 
+    return total_boxes == len(unlocked_boxes)
