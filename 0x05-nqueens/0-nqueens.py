4a194638ecbeaf609b6e131057b9a70d94c2d983
P4ver <s.pales20@gmail.com>
2024-07-04 04:29:47 -0700

    d

--- 0x05-nqueens/0-nqueens.py
+++ 0x05-nqueens/0-nqueens.py
@@ -2,43 +2,34 @@
 """ N queens """
 import sys
 
-
-if len(sys.argv) > 2 or len(sys.argv) < 2:
-    print("Usage: nqueens N")
-    exit(1)
-
-if not sys.argv[1].isdigit():
-    print("N must be a number")
-    exit(1)
-
-if int(sys.argv[1]) < 4:
-    print("N must be at least 4")
-    exit(1)
-
-q = int(sys.argv[1])
-
-
-def queens(q, i=0, a=[], b=[], c=[]):
-    """ find possible positions """
+def queens(q, i=0, a=set(), b=set(), c=set()):
+    """ Find possible positions using backtracking """
     if i < q:
         for j in range(q):
-            if j not in a and i + j not in b and i - j not in c:
-                yield from queens(q, i + 1, a + [j], b + [i + j], c + [i - j])
+            if j not in a and i+j not in b and i-j not in c:
+                yield from queens(q, i+1, a.union({j}), b.union({i+j}), c.union({i-j}))
     else:
-        yield a
-
+        yield tuple(a)
 
 def solve(q):
-    """ solve """
-    k = []
-    i = 0
-    for solution in queens(q, 0):
-        for s in solution:
-            k.append([i, s])
-            i += 1
-        print(k)
-        k = []
-        i = 0
+    """ Solve and print all solutions """
+    solutions = list(queens(q))
+    for solution in solutions:
+        print(solution)
 
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print("Usage: nqueens N")
+        sys.exit(1)
 
-solve(q)
+    try:
+        q = int(sys.argv[1])
+    except ValueError:
+        print("N must be a number")
+        sys.exit(1)
+
+    if q < 4:
+        print("N must be at least 4")
+        sys.exit(1)
+
+    solve(q)
